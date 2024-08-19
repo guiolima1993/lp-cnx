650dcb5234134b40b4ccf908e9f7cab9c70ca070
guiolima1993 <guilhermeolima93@gmail.com>
2024-08-19 14:13:39 -0300

    finalizado a pagina

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Adiciona a configuração para exportação estática
+    trailingSlash: true, // Garante que a URL tenha uma barra final
+    images: {
+      unoptimized: true, // Desativa a otimização de imagens se você não usar `next/image`
+    },
+  };
+  
+  export default nextConfig;
+  
